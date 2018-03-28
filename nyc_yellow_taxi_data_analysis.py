4ab8a71d8368e40a3a0b86af51fdd20904fde3b6
Nishit Patel <nkp3@illinois.edu>
2018-03-28 00:38:16 -0400

    performed EDA

--- nyc_yellow_taxi_data_analysis.py
+++ nyc_yellow_taxi_data_analysis.py
@@ -33,15 +33,22 @@ taxi_2 = 'nyc_yellow_taxi_data_subset_2.csv'
 #taxi_dt_1 = pd.read_csv('C:/Users/NishitP/Desktop/UIUC MCS-DS/CS-498 - Cloud Computing Applications - SPRING 2018/Project/nyc_yellow_taxi_data_subset_1.csv', sep=',')
 taxi_dt = pd.read_csv('C:/Users/NishitP/Desktop/UIUC MCS-DS/CS-498 - Cloud Computing Applications - SPRING 2018/Project/nyc_taxi_data.csv', sep=',')
 
+#initial analysis on data quality and check
 #print dataset dimensions
 print("number of observations:", taxi_dt.shape[0])
 print( "number of variables:", taxi_dt.shape[1])
 print(taxi_dt.shape)
 
+#print variable datatypes
+print(taxi_dt.dtypes)
 
+#print sample data 
 print(taxi_dt.head(10))
 
-# distribution of trip distances
+#print statistics for each variables
+taxi_dt.describe()
+
+# DISTRIBUTION OF TRIP DISTANCES
 
 # define the figure with 2 subplots
 fig,ax = plt.subplots(1,2,figsize = (15,4)) 
@@ -56,7 +63,7 @@ ax[0].set_title('Histogram of Trip Distance')
 # create a vector to contain Trip Distance
 v = taxi_dt.trip_distance 
 # exclude any data point located further than 3 standard deviations of the median point and 
-# plot the histogram with 30 bins
+# plot the histogram with 20 bins
 v[~((v-v.median()).abs()>3*v.std())].hist(bins=20,ax=ax[1], edgecolor='black') # 
 ax[1].set_xlabel('Trip Distance (miles)')
 ax[1].set_ylabel('Count')
@@ -71,8 +78,10 @@ ax[1].plot(np.arange(0,12,.1),600000*pdf_fitted,'r')
 ax[1].legend(['data','lognormal fit'])
 
 plt.show()
+ 
 
-
+# DISTRIBUTION OF TRIP DISTANCE BY HOUR
+s.pairplot(taxi_dt, vars=["fare_amount", "tip_amount"], size=5)
 
 # random stuff for own testing
 y = taxi_dt.fare_amount
@@ -82,8 +91,8 @@ test_frame = taxi_dt[['trip_distance','rate_code','passenger_count','trip_time',
 taxi_df = pd.concat([x,y], axis=1)
 
 scat_mat = pd.plotting.scatter_matrix(x, c=y,marker='o', hist_kwds={'bins':100}, s=100,alpha=0.7)
-#plt.tight_layout()
-plt.savefig('scat_mat.png')
+plt.tight_layout()
+#plt.savefig('scat_mat.png')
 
-plot = s.pairplot(test_frame,kind="reg")
+plot = s.pairplot(test_frame)
 plot
